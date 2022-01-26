b7cde992ffaa4be62f872da290ab103f7c5f487f
Jason Williams <thejasonmichaelwilliams@gmail.com>
2022-01-26 15:01:04 -0500

    commit tree 8996d61ce597a44b32f463486a7ebb753e5f5e60

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  static sanitize(string) {
+    // return string.replace(/[^A-Za-z0-9-']+/g, ' ');
+    return string.replace(/[^A-z0-9' -]|\^/g, '')
+  }
+  static titleize(string)
+  {
+    const noCapWords = ["the", 'a', "an", "but", "of", "and", "for", "at", "by", "from"];
+    const wordArray = string.split(' ');
+    let newString = [];
+    for (let i = 0; i < wordArray.length; i++)
+    {
+      if (i === 0 || !noCapWords.includes(wordArray[i]))
+      {
+        newString.push(this.capitalize(wordArray[i]));
+      }
+      else
+      {
+        newString.push(wordArray[i]);
+      }
+    }
+    return newString.join(' ');
+  }
 }
